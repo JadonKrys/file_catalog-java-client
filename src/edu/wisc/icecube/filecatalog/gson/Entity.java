630a916e6c0856fdf3e75fe69c6b280c6af84dc1
Jan Oertlin <oe@oenet.org>
2016-10-25 13:04:20 -0500

    Added `toString()` method for JSON representation classes

--- src/edu/wisc/icecube/filecatalog/gson/Entity.java
+++ src/edu/wisc/icecube/filecatalog/gson/Entity.java
@@ -1,9 +1,20 @@
 package edu.wisc.icecube.filecatalog.gson;
 
+import com.google.gson.Gson;
+
 public class Entity {
 	private Links _links;
 	
 	public Entity(final Links _links) {
 		this._links = _links;
+	}
+	
+	public Links getLinks() {
+		return _links;
+	}
+	
+	@Override
+	public String toString() {
+		return new Gson().toJson(this);
 	}
 }
