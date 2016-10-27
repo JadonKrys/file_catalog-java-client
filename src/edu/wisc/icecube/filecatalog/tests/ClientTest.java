b2187bd6c950527a2a5263446233c4f329eed6b2
Jan Oertlin <oe@oenet.org>
2016-10-27 12:35:52 -0500

    Added tests

--- src/edu/wisc/icecube/filecatalog/tests/ClientTest.java
+++ src/edu/wisc/icecube/filecatalog/tests/ClientTest.java
@@ -21,6 +21,7 @@ import edu.wisc.icecube.filecatalog.Client;
 import edu.wisc.icecube.filecatalog.ClientException;
 import edu.wisc.icecube.filecatalog.Error;
 import edu.wisc.icecube.filecatalog.Error.BadRequestError;
+import edu.wisc.icecube.filecatalog.Error.ConflictError;
 import edu.wisc.icecube.filecatalog.Error.NotFoundError;
 import edu.wisc.icecube.filecatalog.gson.BasicMetaData;
 import edu.wisc.icecube.filecatalog.gson.Creation;
@@ -30,9 +31,14 @@ import edu.wisc.icecube.filecatalog.gson.FileList;
 public class ClientTest {
 	private static Client client;
 	private static String metadata;
+	private static String metadataReplica;
+	private static String replaceMetadata;
+	private static String replaceBadMetadata;
 	private static String uid;
+	private static String mongoId;
 	private static String createdFile;
 	private static String updateMetadata;
+	private static String updateBadMetadata;
 	private static Gson gson;
 	
 	@BeforeClass
@@ -43,27 +49,55 @@ public class ClientTest {
 		
 		uid = "__test_java_" + new Date();
 		
-		metadata = "{'uid': '" + uid + "', 'locations': ['/path/to/file.dat'], 'checksum': '" + getFakeSHA512(42) + "'}";
+		final String checksum = getFakeSHA512(42);
+		
+		metadata = "{'uid': '" + uid + "', 'locations': ['/path/to/file.dat'], 'checksum': '" + checksum + "'}";
 		metadata = metadata.replace('\'', '"');
+		
+		metadataReplica = "{'uid': '" + uid + "', 'locations': ['http://www.example.com/file.dat'], 'checksum': '" + checksum + "'}";
+		metadataReplica = metadataReplica.replace('\'', '"');
 		
 		updateMetadata = "{'backup': true}";
 		updateMetadata = updateMetadata.replace('\'', '"');
+		
+		updateBadMetadata = "{'mongo_id': 'abcd'}";
+		updateBadMetadata = updateBadMetadata.replace('\'', '"');
+		
+		replaceMetadata = "{'locations': ['/path/to/another/file.dat'], 'checksum': '" + checksum + "'}";
+		replaceMetadata = replaceMetadata.replace('\'', '"');
+		
+		replaceBadMetadata = "{'uid': 'E=mc^2', 'locations': ['/path/to/another/file.dat'], 'checksum': '" + checksum + "'}";
+		replaceBadMetadata = replaceBadMetadata.replace('\'', '"');
 	}
 
 	@Test
-	public void testCreate() throws Error, ClientProtocolException, IOException, URISyntaxException {
+	public void testCreate1() throws Error, ClientProtocolException, IOException, URISyntaxException {
 		final Creation creation = client.create(metadata);
 		
 		createdFile = creation.getFile();
 		
+		mongoId = Client.getMongoIdFromPath(createdFile);
+		
 		assertNotEquals(createdFile, null);
+	}
+	
+	@Test(expected = ConflictError.class)
+	public void testCreate2() throws Error, ClientProtocolException, IOException, URISyntaxException {
+		// Add Replica fails
+		client.create(metadata);
+	}
+	
+	@Test
+	public void testZCreate3() throws Error, ClientProtocolException, IOException, URISyntaxException {
+		// Add replica
+		final Creation creation = client.create(metadataReplica);
+		
+		assertEquals(mongoId, Client.getMongoIdFromPath(creation.getFile()));
 	}
 	
 	@Test
 	public void testGetList1() throws Error, ClientProtocolException, IOException, URISyntaxException {
 		final FileList list = client.getList();
-		
-		System.out.println(list);
 		
 		assertNotEquals(list, null);
 		
@@ -74,7 +108,7 @@ public class ClientTest {
 		// Look for created file
 		boolean found = false;
 		for(BasicMetaData md: list.getEmbedded().getFiles()) {
-			if(md.getMongoId().equals(Client.getMongoIdFromPath(createdFile)) && md.getUid().equals(uid)) {
+			if(md.getMongoId().equals(mongoId) && md.getUid().equals(uid)) {
 				found = true;
 				break;
 			}
@@ -129,32 +163,89 @@ public class ClientTest {
 		assertArrayEquals(new Object[] {createdFile}, list.getFiles());
 	}
 	
+	@SuppressWarnings("unchecked")
 	@Test
-	public void testGet() throws Error, ClientProtocolException, ClientException, IOException, URISyntaxException {
-		client.getByUid(uid);
+	public void testGet1() throws Error, ClientProtocolException, ClientException, IOException, URISyntaxException {
+		final LinkedTreeMap<Object, Object> result = (LinkedTreeMap<Object, Object>) client.getByUid(uid);
+		final LinkedTreeMap<Object, Object> expectation = (LinkedTreeMap<Object, Object>) gson.fromJson(metadata, Object.class);
+		
+		expectation.put("mongo_id", mongoId);
+		result.remove("_links");
+		
+		assertEquals(expectation, result);
+	}
+	
+	@Test(expected = ClientException.class)
+	public void testGet2() throws Error, ClientProtocolException, ClientException, IOException, URISyntaxException {
+		client.getByUid(uid + "does not exist");
+	}
+	
+	@Test(expected = BadRequestError.class)
+	public void testGet3() throws Error, ClientProtocolException, ClientException, IOException, URISyntaxException {
+		client.get("123");
+	}
+	
+	@Test(expected = NotFoundError.class)
+	public void testGet4() throws Error, ClientProtocolException, ClientException, IOException, URISyntaxException {
+		client.get("000000000000000000000000");
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testUpdate1() throws Error, ClientProtocolException, UnsupportedEncodingException, ClientException, IOException, URISyntaxException {
+		final LinkedTreeMap<Object, Object> updatedMetadata = (LinkedTreeMap<Object, Object>) client.updateByUid(uid, updateMetadata);
+		final LinkedTreeMap<Object, Object> expectation = (LinkedTreeMap<Object, Object>) gson.fromJson(metadata, Object.class);
+		
+		final LinkedTreeMap<Object, Object> update = (LinkedTreeMap<Object, Object>) gson.fromJson(updateMetadata, Object.class);
+		
+		expectation.put("mongo_id", mongoId);
+		expectation.putAll(update);
+		updatedMetadata.remove("_links");
+		
+		assertEquals(expectation, updatedMetadata);
+	}
+	
+	@Test(expected = BadRequestError.class)
+	public void testUpdate2() throws Error, ClientProtocolException, UnsupportedEncodingException, ClientException, IOException, URISyntaxException {
+		client.updateByUid(uid, updateBadMetadata);
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testYReplace1() throws Error, ClientProtocolException, UnsupportedEncodingException, ClientException, IOException, URISyntaxException {
+		final LinkedTreeMap<Object, Object> replacedMetadata = (LinkedTreeMap<Object, Object>) client.replaceByUid(uid, replaceMetadata);
+		final LinkedTreeMap<Object, Object> expectation = (LinkedTreeMap<Object, Object>) gson.fromJson(replaceMetadata, Object.class);
+		
+		expectation.put("mongo_id", mongoId);
+		expectation.put("uid", uid);
+		replacedMetadata.remove("_links");
+		
+		assertEquals(expectation, replacedMetadata);
+	}
+	
+	@Test(expected = BadRequestError.class)
+	public void testYReplace2() throws Error, ClientProtocolException, UnsupportedEncodingException, ClientException, IOException, URISyntaxException {
+		client.replaceByUid(uid, replaceBadMetadata);
 	}
 	
 	@Test
-	public void testUpdate() throws Error, ClientProtocolException, UnsupportedEncodingException, ClientException, IOException, URISyntaxException {
-		final LinkedTreeMap<?, ?> updatedMetadata = client.updateByUid(uid, updateMetadata);
-		
-//		final LinkedTreeMap<?, ?> expectation = (LinkedTreeMap<?, ?>) gson.fromJson(metadata, Object.class);
-//		expectation.put("mongo_id", Client.getMongoIdFromPath(createdFile));
+	public void testZDelete0() throws Error, ClientProtocolException, ClientException, IOException, URISyntaxException {
+		client.deleteByUid(uid);
 	}
 	
 	@Test(expected = ClientException.class)
 	public void testZDelete1() throws Error, ClientProtocolException, ClientException, IOException, URISyntaxException {
-		client.deleteByUid(uid + " does not exist");
+		client.deleteByUid(uid);
+	}
+	
+	@Test(expected = BadRequestError.class)
+	public void testZDelete2() throws Error, ClientProtocolException, ClientException, IOException, URISyntaxException {
+		client.delete("bad mongo_id");
 	}
 	
 	@Test(expected = NotFoundError.class)
-	public void testZDelete2() throws Error, ClientProtocolException, ClientException, IOException, URISyntaxException {
-		client.delete("does not exist");
-	}
-	
-	@Test
-	public void testZZFinalDelete() throws Error, ClientProtocolException, ClientException, IOException, URISyntaxException {
-		client.deleteByUid(uid);
+	public void testZDelete3() throws Error, ClientProtocolException, ClientException, IOException, URISyntaxException {
+		client.delete(mongoId);
 	}
 	
 	private static String getFakeSHA512(final long seed) {
